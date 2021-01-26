fead147e6152f51252597622e3e711f5c61bc6ce
Asguerrero <54660849+Asguerrero@users.noreply.github.com>
2021-01-26 16:06:35 -0500

    Update convert.py

--- database/convert.py
+++ database/convert.py
@@ -1,9 +1,6 @@
 import csv
 
 def read_athlete_csv():
-    """
-    Reads athlete_events.csv and returns list.
-    """
     athlete_events = []
 
     with open("athlete_events.csv", mode="r") as file:
@@ -12,6 +9,14 @@ def read_athlete_csv():
             athlete_events.append(line)
 
     return (athlete_events)
+
+def read_noc_csv():
+    noc_regions = []
+    with open("noc_regions.csv", mode="r") as file:
+        csvFile = csv.reader(file, delimiter=",")
+        for line in csvFile:
+            noc_regions.append(line)
+    return (noc_regions)
 
 
 def add_athlete_ids(athlete_events):
@@ -29,9 +34,7 @@ def add_athlete_ids(athlete_events):
             counter = counter + 1
             dict[name] = counter
             row.append(counter)
-            athlete_table.append([row[15], row[1], row[2], row[3], row[4], row[5]])
-
-
+            athlete_table.append([row[15], row[1]])
 
     return athlete_events, athlete_table
 
@@ -80,6 +83,7 @@ def add_events_ids(athlete_events):
 def add_games_ids(athlete_events):
     dict = {}
     counter = 0
+    games_table = []
     for row in athlete_events:
         name = row[8]
         try:
@@ -91,12 +95,15 @@ def add_games_ids(athlete_events):
             dict[name] = counter
             counter = counter + 1
             row.append(counter)
-    return athlete_events
+            games_table.append([row[18],row[8]])
+
+    return athlete_events, games_table
 
 
 def add_medal_ids(athlete_events):
     dict = {}
     counter = 0
+    medals_table =[]
     for row in athlete_events:
         name = row[14]
         try:
@@ -108,13 +115,15 @@ def add_medal_ids(athlete_events):
             dict[name] = counter
             counter = counter + 1
             row.append(counter)
+            medals_table.append([row[19], row[14]])
 
-    return athlete_events
+    return athlete_events, medals_table
 
 
 def add_noc_id(athlete_events):
     dict = {}
     counter = 0
+    noc_table = []
     for row in athlete_events:
         name = row[7]
         try:
@@ -126,9 +135,16 @@ def add_noc_id(athlete_events):
             dict[name] = counter
             counter = counter + 1
             row.append(counter)
-    print(dict)
-    return athlete_events
+            noc_table.append([row[20], row[7]])
 
+    return athlete_events, noc_table
+
+
+def create_csv(table, file_name):
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(table)
+    
 
 def create_all_ids_csv(athlete_events):
     id_fields = []
@@ -154,21 +170,49 @@ def create_events_table(events_table):
         writer = csv.writer(file)
         writer.writerows(events_table)
 
+def create_medal_table(medal_table):
+    with open('medal.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(medal_table)
+
+def create_games_table(games_table):
+    with open('games.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(games_table)
+
+def create_noc_table(noc_table, noc_regions):
+    for row in noc_table:
+        for noc in noc_regions:
+            if row[1] == noc[0]:
+                row.append(noc[1])
+    with open('noc.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(noc_table)
+
 def create_updated_athlete_events(updated_table):
     with open('updated_athlete_events.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(updated_table)
 
 
-athlete_events = read_athlete_csv()
-updated_athlete_events, athlete_table = add_athlete_ids(athlete_events)
-updated_athlete_events, sports_table = add_sport_ids(updated_athlete_events)
-updated_athlete_events, events_table = add_events_ids(updated_athlete_events)
-updated_athlete_events = add_games_ids(updated_athlete_events)
-updated_athlete_events = add_medal_ids(updated_athlete_events)
-updated_athlete_events = add_noc_id(updated_athlete_events)
-create_all_ids_csv(updated_athlete_events)
-create_athlete_table(athlete_table)
-create_sports_table(sports_table)
-create_events_table(events_table)
-create_updated_athlete_events(updated_athlete_events)
+
+
+def main():
+    athlete_events = read_athlete_csv()
+    noc_regions = read_noc_csv()
+    updated_athlete_events, athlete_table = add_athlete_ids(athlete_events)
+    updated_athlete_events, sports_table = add_sport_ids(updated_athlete_events)
+    updated_athlete_events, events_table = add_events_ids(updated_athlete_events)
+    updated_athlete_events, games_table = add_games_ids(updated_athlete_events)
+    updated_athlete_events, medals_table = add_medal_ids(updated_athlete_events)
+    updated_athlete_events, noc_table = add_noc_id(updated_athlete_events)
+    create_all_ids_csv(updated_athlete_events)
+    create_athlete_table(athlete_table)
+    create_sports_table(sports_table)
+    create_events_table(events_table)
+    create_updated_athlete_events(updated_athlete_events)
+    create_medal_table(medals_table)
+    create_games_table(games_table)
+    create_noc_table(noc_table, noc_regions)
+
+main()
